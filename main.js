06becaf0b16c0b4a6fba8ba86a703d21d47e7bfe
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 15:01:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const fs = require('fs');
-const readline = require('readline');
+const path = require('path');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+if (process.argv.length < 4) {
+  console.log('Usage: node app.js <filename> <wordToRemove>');
+  process.exit(1);
+}
 
-rl.question('Enter the filename: ', (filename) => {
-  rl.question('Enter the word to remove: ', (word) => {
-    // TODO: Implement this function
-    rl.close();
+const [filename, wordToRemove] = process.argv.slice(2);
+
+const filePath = path.join(__dirname, filename);
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading the file: ${err}`);
+    return;
+  }
+
+  const regex = new RegExp(`\\b${wordToRemove}\\b`, 'g');
+  const modifiedData = data.replace(regex, '');
+
+  fs.writeFile(filePath, modifiedData, 'utf8', err => {
+    if (err) {
+      console.error(`Error writing to the file: ${err}`);
+      return;
+    }
+
+    console.log(`Removed ${wordToRemove} from ${filename}`);
   });
 });
